e8ccb250d0ed21c2fe4dca3bb09bfea08fe96c85
jadwanigaurav <gaurav.jadwani@delhivery.com>
2021-10-15 00:59:39 +0530

    export added.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 const axios = require('axios');
 
-axios.get('https://reactjs.org/docs/hooks-rules.html').then(function(da){
-    console.log('da',da)
-}).catch(function(){
-    console.log('fe')
-})
+function see(){
+    axios.get('https://reactjs.org/docs/hooks-rules.html').then(function(da){
+        console.log('da',da)
+    }).catch(function(){
+        console.log('fe')
+    })
+}
+module.exports = see;
